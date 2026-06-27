f9e168ae6318cbe0eb9395a27b5c3f2f4c1b3324
Dhanush552005 <dhanushshankar555@gmail.com>
2026-06-27 19:23:27 +0530

    ready to deploy

--- backend/download_models.py
+++ backend/download_models.py
@@ -1,18 +1,15 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
-import gdown
 
 BASE_DIR = Path(__file__).resolve().parent
-MODEL_DIR = BASE_DIR / "ml_models"
-
-MODEL_DIR.mkdir(parents=True, exist_ok=True)
+load_dotenv(BASE_DIR / ".env")
 
 MODELS = {
-    "yolo.pt": "10GiihatLuBQapwUMBKMbZ4mcxZqjwX90",
-    "cnn_model.h5": "1n7iMgzsT5-ppmNxY2WKLPbixi-T6oSzS",
-    "xgboost_resale_model.pkl": "1DPQC_0mATwrII0I9UR8ZgpCdGcajaiKi",
+    "yolo.pt": os.getenv("YOLO_FILE_ID"),
+    "cnn_model.h5": os.getenv("CNN_FILE_ID"),
+    "xgboost_resale_model.pkl": os.getenv("XGB_FILE_ID"),
 }
-
 def download_models():
     for filename, file_id in MODELS.items():
         destination = MODEL_DIR / filename
